cbb1c622052dcd8bd084c48695d01e261c753529
Minh Pham <phamduyanminh@users.noreply.github.com>
2020-07-18 12:37:48 -0400

    Finish carousel and edited some css.

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function test(){
 
 window.addEventListener('scroll', function(){
     const navElement = this.document.getElementById("nav-js")
-    const bannerElement = this.document.getElementById("banner-js")
-    const totalHeight = navElement.offsetHeight + bannerElement.offsetHeight //Calculate total height of 2 elements
+    const carouselElement = this.document.getElementById("carouselExampleCaptions")
+
+    //Calculate total height of 2 elements
+    const totalHeight = navElement.offsetHeight + carouselElement.offsetHeight 
     const currentHeight = document.documentElement.scrollTop
 
     if(currentHeight >= totalHeight - 350){
